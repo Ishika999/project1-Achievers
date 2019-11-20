c46c65898fc929e0cfe690942a0e7d124c530a25
Ishika999 <57324906+Ishika999@users.noreply.github.com>
2019-11-20 16:11:41 +0530

    java file created

--- DuplicateElements.java
+++ DuplicateElements.java
@@ -1,7 +1,6 @@
 import java.util.*;
 public class DuplicateElements {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		int arr[]= new int[n];
@@ -10,18 +9,17 @@ public class DuplicateElements {
 			arr[i]=sc.nextInt();
 		}
 		int count=1;
-		boolean visited[]=new boolean[arr.length];
-		Arrays.fill(visited,false);
+		boolean arr1[]=new boolean[arr.length];
 		for(int i=0;i<arr.length;i++)
 		{
-			if(visited[i]==true)
+			if(arr1[i]==true)
 				continue;
 			count=1;
 			for(int j=i+1;j<arr.length;j++)
 			{
 				if(arr[i]==arr[j])
 					{
-						visited[j]=true;
+						arr1[j]=true;
 						count++;
 					}
 			}
@@ -29,5 +27,5 @@ public class DuplicateElements {
 			System.out.println(arr[i]+" comes: "+count);
 		}
 	}
-
 }
+
